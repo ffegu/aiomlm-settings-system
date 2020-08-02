0bbcb400b0026f355ea685a6341ef77287775003
Pran pegu <ffegu0617@gmail.com>
2020-08-02 15:24:44 +0530

    first commit

---
+++ src/SettingsServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Aiomlm\Settings;
+use Illuminate\Support\ServiceProvider;
+/**
+ *
+ */
+class SettingsServiceProvider extends ServiceProvider
+{
+
+   public function boot()
+   {
+       dd('hello');
+   }
+
+   public function register()
+   {
+
+   }
+}
